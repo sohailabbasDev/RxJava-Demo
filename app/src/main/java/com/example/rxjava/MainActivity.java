575f3aa002ca19e3b8b443b64118c3d1ebb1c72e
iamChintan <88285492+iamChintan@users.noreply.github.com>
2023-03-21 22:43:59 -0400

    RxJava Methods

--- app/src/main/java/com/example/rxjava/MainActivity.java
+++ app/src/main/java/com/example/rxjava/MainActivity.java
@@ -3,12 +3,53 @@ package com.example.rxjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList animals = new ArrayList();
+        animals.add("Lion");
+        animals.add("Tiger");
+        animals.add("Cat");
+        animals.add("Elephant");
+
+        Observable.just(animals)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new Observer<ArrayList>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.e(TAG, "onSubscribe: " + d.toString() );
+                    }
+
+                    @Override
+                    public void onNext(ArrayList arrayList) {
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 }
